10bb6ec593c649d4efd2ddae846194cb31a1ddb3
Kyaz-U <umaruktamov2023@gmail.com>
2025-04-22 22:01:42 +0500

    Update predict_model.py

--- predict_model.py
+++ predict_model.py
@@ -2,7 +2,12 @@ import pickle
 import numpy as np
 
 def predict_signal(last_3):
-    with open("models/aviator_model.pkl", "rb") as f:
-        model = pickle.load(f)
-    input_data = np.array(last_3).reshape(1, -1)
-    return model.predict_proba(input_data)[0][1]  # Ehtimol: 1.80x+
+    try:
+        with open("models/aviator_model.pkl", "rb") as f:
+            model = pickle.load(f)
+        input_data = np.array(last_3).reshape(1, -1)
+        probability = model.predict_proba(input_data)[0][1]  # ehtimol 1.80x+
+        return probability
+    except Exception as e:
+        print("Xatolik modelni yuklashda:", e)
+        return 0.0
