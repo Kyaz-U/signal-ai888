d7818d0cd6dd273038a55fdd735d2c12af256136
Kyaz-U <umaruktamov2023@gmail.com>
2025-04-23 00:39:56 +0500

    Update predict_model.py

--- predict_model.py
+++ predict_model.py
@@ -1,17 +1,10 @@
-import pandas as pd
-import numpy as np
 import pickle
-import os
-
-MODEL_PATH = "models/aviator_model.pkl"
-
-def load_model():
-    with open(MODEL_PATH, "rb") as f:
-        model = pickle.load(f)
-    return model
+import numpy as np
 
 def predict_signal(k1, k2, k3):
-    model = load_model()
-    input_data = pd.DataFrame([[k1, k2, k3]], columns=["k1", "k2", "k3"])
-    probability = model.predict_proba(input_data)[0][1]
-    return round(probability * 100, 2)
+    with open("models/aviator_model.pkl", "rb") as file:
+        model = pickle.load(file)
+    
+    X = np.array([[k1, k2, k3]])
+    probability = model.predict_proba(X)
+    return float(probability[0][1])  # 1.80x+ ehtimoli
